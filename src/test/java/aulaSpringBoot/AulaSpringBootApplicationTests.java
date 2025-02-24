514636a8b4a635e04309958e6031b1553ead21c0
marcellofelisbino <smarcfs@gmail.com>
2025-02-24 10:48:06 -0300

    Criacao projeto

---
+++ src/test/java/aulaSpringBoot/AulaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package aulaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
