c0a76e731e0cc10b9ee31041b5b3e94034af19b4
Marcellofelisbino <smarcfs@gmail.com>
2025-03-07 17:27:50 -0300

    Add files via upload

---
+++ AulaSpringBootAplicacao.java
@@ -0,0 +1,13 @@
+package aulaSpringBoot.APLICACAO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringBootAplicacao {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringBootAplicacao.class, args);
+	}
+
+}
